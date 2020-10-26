b3ab436037028d2dba34041187debcb1afe350d2
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2020-10-26 16:21:00 +0530

    Update and rename ListInterface.java to List_collection_Interface.java

--- List_collection_Interface.java
+++ List_collection_Interface.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
 
-public class ListInterface {
+public class List_collection_Interface {
 
     public static void main(String[] args) {
 
